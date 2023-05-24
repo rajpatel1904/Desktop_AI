f60250aca19fd8cb88dce4ec657c71830484e976
Raj <r_patel46@u.pacific.edu>
2023-05-24 00:22:17 -0700

    when user speak some sentence after theat AI repeat a sentence

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import speech_recognition as s
 import pyttsx3
 
-def speak(text):
+
+def speak( text):
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
 
+
+def takeWord():
+    r = s.Recognizer()
+    with s.Microphone() as source:
+        r.pause_threshold = 1
+        a = r.listen(source)
+        q = r.recognize_google(a, language='en-in')
+        print(f'{q}')
+        return q
+
+
 if __name__ == '__main__':
     print('Pycharm')
     speak('Hello, I am Desktop A.I.')
+    text = takeWord()
+    speak(text)
